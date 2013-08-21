67de4bd211da2fede1bf3500efbff36449ffccd5
Tholkappiyan <tholkappiyan@Tholkappiyans-MacBook-Pro.local>
2013-08-21 17:59:52 +0530

    redis key list

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,3 +1,5 @@
-# config = YAML::load_file(File.join(RAILS_ROOT, 'config', 'redis.yml'))[RAILS_ENV]
+config = YAML::load_file(File.join(RAILS_ROOT, 'config', 'redis.yml'))[RAILS_ENV]
 
 # $redis = Redis.new(:host => config["host"], :port => config["port"])
+
+$redis = Redis.new(:host => config["host"], :port => config["port"])

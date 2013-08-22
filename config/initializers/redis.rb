5859360ef1d4cbd39e0d387726d134107d091c8c
Tholkappiyan <tholkappiyan@Tholkappiyans-MacBook-Pro.local>
2013-08-22 16:44:13 +0530

    Redis enabled

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,3 +1,3 @@
-# config = YAML::load_file(File.join("#{File.dirname(__FILE__)}/../..", 'config', 'redis.yml'))
+config = YAML::load_file(File.join("#{File.dirname(__FILE__)}/../..", 'config', 'redis.yml'))
 
-# $redis = Redis.new(:host => config["host"], :port => config["port"])
+$redis = Redis.new(:host => config["host"], :port => config["port"])

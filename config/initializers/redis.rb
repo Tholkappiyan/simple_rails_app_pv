66108f316228497f9a03bcc672b7917043b6f37a
Tholkappiyan <tholkappiyan@Tholkappiyans-MacBook-Pro.local>
2013-08-22 12:34:46 +0530

    Modified config/initializers/redis.rb

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,4 +1,4 @@
-config = YAML::load_file(File.join(RAILS_ROOT, 'config', 'redis.yml'))[RAILS_ENV]
+config = YAML::load_file(File.join("#{File.dirname(__FILE__)}/../..", 'config', 'redis.yml'))[RAILS_ENV]
 
 # $redis = Redis.new(:host => config["host"], :port => config["port"])
 
